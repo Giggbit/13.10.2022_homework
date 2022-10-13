81f5433a81615811d87f97a9ab3a753ed35af354
Giggbit <danya19981017@gmail.com>
2022-10-13 10:43:52 +0200

    Добавьте файлы проекта.

---
+++ Parrot.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+#include "Animal.h"
+using namespace std;
+
+class Parrot : public Animal, public IFly {
+
+public:
+	Parrot() = default;
+	Parrot(string n, double k, string c) :Animal(n, k, c) {}
+
+	// ----- реализация ИНТЕРФЕЙСА IANIMAL !!!
+	virtual void Eat() {
+		cout << "Parrot food: seeds\n";
+	}
+	virtual void Move() {
+		cout << "Move: mid movement speed\n";
+	}
+
+	// ----- реализация ИНТЕРФЕЙСА IFLY !!!
+	virtual void Fly() {
+		cout << "Fly: average flight altitude\n";
+	}
+};
+
